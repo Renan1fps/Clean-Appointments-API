fc932f8f3c0188f1f4e0f4b188eba5c88f8c3524
Renan1fps <renan.sergipe68@gmail.com>
2022-10-09 23:46:48 -0300

    chore: config jest

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  collectCoverage: true,
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/src/(.*)': '<rootDir>/src/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+};
